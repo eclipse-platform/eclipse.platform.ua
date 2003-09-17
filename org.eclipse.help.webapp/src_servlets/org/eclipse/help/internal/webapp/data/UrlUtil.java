51afded160ace15b04386efedac4818725284d85
Konrad Kolosowski <kkolosow>
2003-09-17 21:27:43 +0000

    43230 Infocenter: UI labels in different language than content

--- org.eclipse.help.webapp/src_servlets/org/eclipse/help/internal/webapp/data/UrlUtil.java
+++ org.eclipse.help.webapp/src_servlets/org/eclipse/help/internal/webapp/data/UrlUtil.java
@@ -270,11 +270,13 @@ public class UrlUtil {
 			return new Locale(
 				localeStr.substring(0, 2),
 				localeStr.substring(3, 5));
-		} else {
+		} else if (localeStr.length() >= 2) {
 			return new Locale(localeStr.substring(0, 2), "");
-
+		} else {
+			return Locale.getDefault();
 		}
 	}
+	
 	public static String getLocale(HttpServletRequest request) {
 		if (defaultLocale == null) {
 			initializeLocales();

7c4d100f0a3f43c9eea5a70157a8c22f4e055475
Mazen Faraj <mraraj>
2004-08-05 16:19:47 +0000

    update apis

--- org.eclipse.ui.intro/src/org/eclipse/ui/intro/config/IIntroContentProvider.java
+++ org.eclipse.ui.intro/src/org/eclipse/ui/intro/config/IIntroContentProvider.java
@@ -18,12 +18,35 @@ import org.eclipse.ui.forms.widgets.*;
 /**
  * A content provider for dynamic intro content. It is initialized with the
  * content provider site because a typical a content provider would need to
- * update its contents dynamically at runtime.
+ * update its contents dynamically at runtime. And so, the site can be informed
+ * of a need to redraw its content through a call to its reflow method.
+ * <p>
+ * The life cycle of an IIntroContentProvider is as follows:
+ * <ul>
+ * <li>a content provider is defined in the Intro content markup file (ie:
+ * introContent.xml fiule) as follows:
  * 
- * @since 3.0.1
+ * <p>
+ * &lt;contentProvider id=&quot;roles&quot;
+ * class=&quot;x.y.z.ContentProvider&quot;&gt; <br>
+ * &lt;text&gt;Some alternate text for dynamic content&lt;/text&gt; <br>
+ * &lt;/contentProvider&gt;
+ * </p>
+ * 
+ * This defines the content provider as part of the intro content for that page.
+ * </li>
+ * <li>init() is called to initialize the instance of the class on load of the
+ * page. This is where the provider site can be cacched for later reuse.</li>
+ * <li>then createContent is actually called to give the content provider a
+ * chance to generate the dynamic content. This is when the dynamic content can
+ * be cached for later reuse when the same page is shown again.</li>
+ * <li>finally, when the intro view is closed, dispose will be called on all
+ * this content provider to give it a chance to clean up.</li>
+ * 
+ * @since 3.0.1 
  */
 public interface IIntroContentProvider {
-	/**
+    /**
 	 * Initializes the content provider. An IIntroContentProviderSite is passed,
 	 * which will be called on to recompute or layout the content when the
 	 * content becomes stale.
@@ -31,9 +54,10 @@ public interface IIntroContentProvider {
 	 * @param site
 	 *            the site of this IIntroContentProvider
 	 */
-	public void init(IIntroContentProviderSite site);
+    public void init(IIntroContentProviderSite site);
 
-	/**
+    
+    /**
 	 * Creates HTML content in the provided PrintWriter. This content will be
 	 * included in the generated HTML page when embedded HTML widget is used to
 	 * render intro content.
@@ -47,7 +71,7 @@ public interface IIntroContentProvider {
 	 */
 	public void createContent(String id, PrintWriter out);
 
-	/**
+    /**
 	 * Creates SWT content in the provided Composite. This method is called when
 	 * Eclipse Forms are used to render intro content.
 	 * 
@@ -59,11 +83,24 @@ public interface IIntroContentProvider {
 	 * @param toolkit
 	 *            the form toolkit that should be used when creating new widgets
 	 */
-	public void createContent(String id, Composite parent, FormToolkit toolkit);
+    public void createContent(String id, Composite parent, FormToolkit toolkit);
 
-	/**
-	 * Disposes of the content provider
-	 */
-	public void dispose();
+
+    /**
+     * Returns the id of the Intro contentProvider tag that defined this content
+     * provider. This can/should be cached from the init() method above and
+     * returned by this method.
+     * 
+     * @return
+     */
+    public String getId();
+
+    /**
+     * Dispose of the ContentProvider. This will only be called when the Intro
+     * view is closed. In other words, the content provider will not be disposed
+     * of until the last possible minute. This gives the implementor the chance
+     * to cache content and avoid regenerating content on every page switch.
+     */
+    public void dispose();
 
 }

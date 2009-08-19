58fcfdedb883201e69c82cd304f5a92dc14ef50b
Chris Goldthorpe <cgoldthor>
2009-08-19 19:54:46 +0000

    Bug 103301  [Webapp] Search highlighting hangs for awhile on some pages in Internet Explorer

--- org.eclipse.help.webapp/advanced/highlight.js
+++ org.eclipse.help.webapp/advanced/highlight.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,7 +47,7 @@ function highlight(){
 	}
 	currentHighlight = defaultHighlight;
 	
-	if(document.body.innerHTML.length < 50000){
+	if(document.body.innerHTML.length < 50000 && !isIE){
 		for(i=0; i<keywords.length; i++){
 		    firstNodeHighlighted=false;
 			word=keywords[i].toLowerCase();

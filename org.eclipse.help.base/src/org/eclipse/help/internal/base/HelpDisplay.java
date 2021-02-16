d671d742159d0c15a6c69acfb334f11299e2aea2
Andrew Johnson <andrew_johnson@uk.ibm.com>
2021-02-16 21:40:49 +0000

    569855: Fix for Eclipse live help. - Use tokens - Backport to R4_5_maintenance branch
    Change-Id: I09cee753dc24d55a2704b80ae13d392d233aac0d
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=569855
    Signed-off-by: Andrew Johnson <andrew_johnson@uk.ibm.com>(cherry picked from commit 90661664f30d4e1225e4a68b179c6ee110cd7f75)
    Also-by: Niraj Modi <niraj.modi@in.ibm.com>

--- org.eclipse.help.base/src/org/eclipse/help/internal/base/HelpDisplay.java
+++ org.eclipse.help.base/src/org/eclipse/help/internal/base/HelpDisplay.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,8 @@ package org.eclipse.help.internal.base;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -190,6 +192,12 @@ public class HelpDisplay {
 			} else if (helpURL.startsWith("topic=")) { //$NON-NLS-1$
 				String topic = helpURL.substring("topic=".length()); //$NON-NLS-1$
 				helpURL = getHelpDisplay().getHelpForTopic( topic, WebappManager.getHost(),  WebappManager.getPort());
+			}
+			String basehelp = getBaseURL();
+			if (BaseHelpSystem.getMode() != BaseHelpSystem.MODE_INFOCENTER && helpURL.startsWith(basehelp)) {
+				String sessid = UUID.randomUUID().toString();
+				BaseHelpSystem.getInstance().setLiveHelpToken(sessid);
+				helpURL += (helpURL.indexOf('?') < 0 ? '?' : '&') + "token=" + sessid; //$NON-NLS-1$
 			}
 
 			BaseHelpSystem.getHelpBrowser(forceExternal)
--- org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/LayoutData.java
+++ org.eclipse.help.webapp/src/org/eclipse/help/internal/webapp/data/LayoutData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -43,6 +43,11 @@ public class LayoutData extends RequestData {
 
 		// initialize the query string
 		String qs = request.getQueryString();
+		// Remove any live help token
+		if (qs != null) {
+			qs = qs.replaceFirst("^token=[a-z0-9-]{36}", ""); //$NON-NLS-1$ //$NON-NLS-2$
+			qs = qs.replaceFirst("&token=[a-z0-9-]{36}", ""); //$NON-NLS-1$ //$NON-NLS-2$
+		}
 		if (qs != null && qs.length() > 0)
 			query = "?" + qs; //$NON-NLS-1$
 	}
